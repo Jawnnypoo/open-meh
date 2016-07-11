09b7abbf8e3ef1ce082d8a2560059fa4b98a4ff5
Jawnnypoo <jawnnypoo@gmail.com>
2016-07-11 16:12:28 -0500

    Fix travis

--- scripts/prepare-build.sh
+++ scripts/prepare-build.sh
@@ -2,4 +2,4 @@
 # This will make the build work from the command line even if you do not have the proper
 # credentials. No promises on the app actually loading anything though!
 chmod 777 gradlew
-printf "OPEN_MEH_MEH_API_KEY = \"\"\n" > gradle.properties
+echo "OPEN_MEH_MEH_API_KEY = \"\"\n" >> gradle.properties

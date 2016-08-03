ec2c99b14aa575f6d57955759cbd5d3d34ac796a
Jawnnypoo <jawnnypoo@gmail.com>
2016-08-03 17:50:19 -0500

    Allow for building without meh api key cause it was annoying me

--- prepare-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This will make the build work from the command line even if you do not have the proper
-# credentials. No promises on the app actually loading anything though!
-chmod 777 gradlew
-echo "OPEN_MEH_MEH_API_KEY = \"\"\n" >> gradle.properties

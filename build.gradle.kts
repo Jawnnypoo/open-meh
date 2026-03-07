71004e96f8eef45e0ed8b046473da26b659bb64e
John Carlson <jawnnypoo@gmail.com>
2026-03-07 03:38:01 -0600

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
 plugins {
     id("com.android.application") version "9.1.0" apply false
     id("org.jetbrains.kotlin.jvm") version "2.3.10" apply false
-    id("com.github.ben-manes.versions") version "0.52.0"
-}
-
-tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
 }
